9d7171abffee1237051fa577ef8ba3433bf534b3
Shilo Liato <shiloliato@gmail.com>
2026-07-05 13:17:55 +0300

    Remove unused Link import causing Vercel build failure

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
